fa1e2b1e92591912228892ddd81ba943f32a8205
timolor <timolordave@gmail.com>
2020-10-25 00:46:41 +0100

    area code migrations

---
+++ Migrations/20201024234606_added AreaCode to Region.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AirandWebAPI.Migrations
+{
+    public partial class addedAreaCodetoRegion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AreaCode",
+                table: "Regions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AreaCode",
+                table: "DispatchRequestInfos",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AreaCode",
+                table: "Regions");
+
+            migrationBuilder.DropColumn(
+                name: "AreaCode",
+                table: "DispatchRequestInfos");
+        }
+    }
+}
