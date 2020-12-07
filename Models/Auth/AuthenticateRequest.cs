6c54759544cd1c7fa915447acd631645a0ab8fd7
timolor <timolordave@gmail.com>
2020-12-07 14:52:44 +0100

    fixed authentication token for registration

--- Models/Auth/AuthenticateRequest.cs
+++ Models/Auth/AuthenticateRequest.cs
@@ -4,10 +4,17 @@ namespace AirandWebAPI.Models.Auth
 {
     public class AuthenticateRequest
     {
+        //public AuthenticateRequest(){}
+        
         [Required]
         public string Username { get; set; }
 
         [Required]
         public string Password { get; set; }
+
+        public AuthenticateRequest(string username, string password){
+            this.Username = username;
+            this.Password = password;
+        }
     }
 }
